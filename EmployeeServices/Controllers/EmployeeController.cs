03ef0038aff7477fc259e9e61549d5f756d5cbf9
Kalyani <kalyani.kandunuri@thomsonreuters.com>
2020-08-20 11:35:09 +0530

    Add project files.

---
+++ EmployeeServices/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace EmployeeServices.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<EmployeeTable> Get()
+        {
+            using (EmployeeDetailsEntities entities = new EmployeeDetailsEntities())
+            {
+                return entities.EmployeeTables.ToList();
+            }
+        }
+
+        public EmployeeTable Get(int ID)
+        {
+            using (EmployeeDetailsEntities entities =new EmployeeDetailsEntities())
+            {
+                return entities.EmployeeTables.FirstOrDefault(e=> e.ID==ID);
+                
+            }
+        }
+    }
+}
